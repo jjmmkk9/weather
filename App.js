dce27970c559eaa65b13c7ed6b58fd8e82414a51
unomic <>
2023-03-31 13:55:06 +0900

    start

--- App.js
+++ App.js
@@ -1,20 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{flex:1}}>
+      <View style={{flex: 1,  backgroundColor:'black'}}></View>
+      <View style={{flex: 2,  backgroundColor:'red'}}></View>
+      <View style={{flex: 1,  backgroundColor:'yellow'}}></View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+//   text:{
+//     fontSize:28,
+//     color:'black'
+//   }
+// });
